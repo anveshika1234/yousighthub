5486d89e059bdf4e5fd696e22a47b0b66dbe4207
anveshika1234 <64398887+anveshika1234@users.noreply.github.com>
2020-04-28 01:10:38 -0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,12 +5,15 @@ require 'vendor/autoload.php';
 
 // If you are not using Composer (recommended)
 // require("path/to/sendgrid-php/sendgrid-php.php");
-
-$from = new SendGrid\Email(null, "anveshikagbu@gmail.com");
-$subject = "Hello World from the SendGrid PHP Library!";
-$to = new SendGrid\Email(null, "anveshikagbu@gmail.com");
+$name = $_POST['fullname']; 
+$email = $_POST['email']; 
+$phone = $_POST['phone']; 
+$message = $_POST['message'];
+$from = new SendGrid\Email(null, $email);
+$subject = "Submission of new customer!";
+$to = new SendGrid\Email(null, "yoursighthub@gmail.com");
 $content = new SendGrid\Content("text/plain", "Hello, Email!");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$mail = new SendGrid\Mail($from, $subject, $to, $fullname,$phone, $message);
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
