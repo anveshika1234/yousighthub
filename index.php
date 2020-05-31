e730c8d7da1c38f1b0eb22da7b0cf5ef8818a433
anveshika1234 <64398887+anveshika1234@users.noreply.github.com>
2020-05-31 05:23:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 include_once('index.html');
-if (!isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
-    ob_start();            
-}
-elseif (strpos(' ' . $_SERVER['HTTP_ACCEPT_ENCODING'], 'x-gzip') == false) {
-    if (strpos(' ' . $_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') == false) {
-        ob_start();
-    }
-    elseif(!ob_start("ob_gzhandler")) {
-        ob_start();
-    }   
-}
-elseif(!ob_start("ob_gzhandler")) {
-    ob_start();
-}
 
 ?>
 
